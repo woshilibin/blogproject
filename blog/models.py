8bc597122127a63fa7642359306f259829af8a80
woshilibin <qishi_bin@qq.com>
2018-01-11 20:27:18 +0800

    添加摘要

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 # Create your models here.
 class Category(models.Model):
@@ -36,3 +37,13 @@ class Post(models.Model):
     def increase_views(self):
         self.views +=1
         self.save(update_fields=['views'])
+
+    def save(self, *args,**kwargs):
+        if not self.excerpt:
+
+            md=markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt=strip_tags(md.convert(self.body))[:54]
+        super(Post,self).save(*args,**kwargs)
